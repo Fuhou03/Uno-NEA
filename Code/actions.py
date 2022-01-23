2bbf2a131e5f63c89ccb7b3d7f86ae75c913dbf7
Fuhou03 <68759675+Fuhou03@users.noreply.github.com>
2022-01-23 19:19:37 +0000

    Removed player variables + minor fixes

--- Code/actions.py
+++ Code/actions.py
@@ -3,63 +3,61 @@ class Response:
         self.game = game    # the new game state
         self.payload = payload      # payload -  might be None - this is for additional data
 
-class PlaceCard:
+
+class Decision:
     def __init__(self, decision):
         self.decision = decision
 
     def execute(self, game):
-        if self.decision == "y":
-            game.discard_pile.append(current_player.deck[-1])
-            game.current_player.deck.pop(-1)
-            return Response(game, "executed")
-        else:
+        if self.decision == "y":    # If they chose to place down the card that they picked up
+            game.discard_pile.append(game.player_list[game.turn].deck[-1])
+            game.player_list[game.turn].deck.pop(-1)
+            return Response(game, "Executed")
+
+        else:   # Moves onto the next player without placing the card down
+            game.turn = game.next_turn()
             return Response(game, None)
 
-class CheckCard:
+
+class PlaceCard:
     def __init__(self, choice, **kw):
         self.choice = choice    # A number representing the player's choice
         self.colour = kw.get("colour")
 
     def execute(self, game):
-        current_player = game.player_list[game.turn]    # Changes every turn
+        #if (game.player_list[game.turn].deck[self.choice].colour == game.discard_pile[-1].colour) or \
+        #        (game.player_list[game.turn].deck[self.choice].value == game.discard_pile[-1].value):
+        #
+        #    game.discard_pile.append(game.player_list[game.turn].deck[self.choice])
+        #    game.player_list[game.turn].deck.pop(self.choice)
+        #    return Response(game, "Executed")
 
-        # If my choice matches correctly
-        if (current_player.deck[self.choice].colour == game.discard_pile[-1].colour) or \
-                (current_player.deck[self.choice].value == game.discard_pile[-1].value):
-            game.discard_pile.append(current_player.deck[self.choice])  # Add the card to the discard pile
+        if game.player_list[game.turn].deck[self.choice].colour == None:      # If they chose a wild card
+            # Replaces the wild card's 'None' colour with the colour chosen and places that card down
+            game.player_list[game.turn].deck[self.choice].colour = self.colour
 
-            current_player.deck.pop(self.choice)  # Remove it from your deck
-            return Response(game, "executed")
+        game.discard_pile.append(game.player_list[game.turn].deck[self.choice])     # Add the card to the discard pile
+        game.player_list[game.turn].deck.pop(self.choice)       # Remove it from your deck
 
-        elif current_player.deck[self.choice].value == "wild" or\
-                current_player.deck[self.choice].value == "wild 4":
-            # If they chose a wild card - the colour of the card at the top of the pile changes
-            current_player.deck[self.choice].colour = self.colour
-            game.discard_pile.append(current_player.deck[self.choice])
-
-            current_player.deck.pop(self.choice)
-            return Response(game, "executed")
-
+        return Response(game, "Executed")
 
 
 class DrawCard:
     def __init__(self):
-        pass
+        self.temp = None
 
     def execute(self, game):
-        current_player = game.player_list[game.turn]
+        # The draw card method from the Deck object inside the game object is called
+        game.player_list[game.turn].deck = game.dk.draw_card(game.player_list[game.turn].deck)  # Draw 1 card
 
-        current_player.deck = game.dk.draw_card(current_player.deck)  # Draw 1 card from main deck
-
-        # If the card you drew is valid you can place it down immediately
-        if current_player.deck[-1].colour == game.discard_pile[-1].colour or \
-                current_player.deck[-1].value == game.discard_pile[-1].value:
+        # If the card you drew is valid it asks the user if they want to place it down immediately -
+        if game.player_list[game.turn].deck[-1].colour == game.discard_pile[-1].colour or \
+                game.player_list[game.turn].deck[-1].value == game.discard_pile[-1].value:
             return Response(game, "confirm")
 
-        else:   # If they card they drew cannot be placed down, it moves on
+        else:   # If they card they drew cannot be placed down, it increments the turn so the game moves on
             game.turn = game.next_turn()
             return Response(game, None)
-            #self.move_on = True
 
 
 
