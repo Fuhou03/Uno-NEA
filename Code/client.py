d74ef208d5b2ae93b3a10ce9311d33805c34de2a
Fuhou03 <68759675+Fuhou03@users.noreply.github.com>
2022-02-15 17:11:50 +0000

    Game now moves onto next player

--- Code/client.py
+++ Code/client.py
@@ -16,7 +16,7 @@ def main():
             while True:
                 interface.game_mode.waiting_screen()    # Displays the waiting screen
 
-                if net.receive():   # Receives "Started" when the game has begun
+                if net.receive():   # Receives "Started" when the game has begun to stop the loop
                     interface.running = False
                     player_number = net.receive()   # Receive your player number from the server
                     break
@@ -26,44 +26,48 @@ def main():
     while running:
         try:
             state = net.receive()
+            print("Received Data")
 
-        except:
+        except Exception as e:
+            print(e)
             print("\nRan into an issue when receiving the data.")
             running = False
 
-        else:
-            if state.game.turn == player_number:    # If it's your turn you perform an action
-                interface.game_screen.action = None     # Reset the action
 
-                if state.payload == "choose":
-                    while not interface.card_chosen:   # Client hasn't chosen card yet
-                        print(player_number)
-                        interface.game_screen.display(player_number, state.game) # To display the game screen
+        if state.game.turn == player_number:    # If it's your turn you perform an action
+            if state.payload == "Choose":
+                
+                while not interface.card_chosen:   # Client hasn't chosen card yet
+                    interface.game_screen.display(player_number, state.game) # To display the game screen
+                interface.card_chosen = False   # Reset it so they can choose another card next turn
 
-                    interface.card_chosen = False   # Reset it so they can choose another card next turn
+            elif state.payload == "Confirm":
+                
+                interface.game_screen.confirm = True
+                while interface.game_screen.confirm:
+                    interface.game_screen.ask()     # Asks the user if they want to place the drawn card down
 
-                elif state.payload == "Confirm":
-                    interface.game_screen.confirm = True
-
-                    while interface.game_screen.confirm:
-                        interface.game_screen.ask()     # Asks the user if they want to place the drawn card down
-
-                #elif state.payload == "Executed" or state.payload == None:
-                    # None when they don't place down the card they picked up. "Executed" if the card was placed down.
-
+            if state.payload == "Choose" or state.payload == "Confirm":
+                
+                # None when they don't place down the card they picked up. "Executed" if the card was placed down.
                 action = interface.game_screen.action
                 net.send(action)
+                interface.game_screen.action = None     # Reset the action
                 alert = net.receive()   # Tells the client that the action was executed
 
-            else:   # If it's not your turn
-                while True:
-                    print(player_number)    # Received "Executed" here for p3
-                    interface.game_screen.display(player_number, state.game)
+            print("Client moved on") # Testing
 
+        else:   # If it's not your turn
+            if state.payload == "Choose":   # Displays the game but doesn't allow them to pick a card
+                while True:
+                    print(player_number)
+                    interface.game_screen.display(player_number, state.game)
                     if net.receive():   # When a player has made a move you receive data
                         break   # To return to the main loop
-
-
+                    else:
+                        continue
+            else:
+                print("Moved On") # Testing
 
 if __name__ == "__main__":
     main()
