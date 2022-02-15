6c2b916507335968fb7cd7ac04696b4fd107a29c
Fuhou03 <68759675+Fuhou03@users.noreply.github.com>
2022-02-15 17:13:55 +0000

    Update actions.py

--- Code/actions.py
+++ Code/actions.py
@@ -9,7 +9,7 @@ class Decision:
         self.decision = decision
 
     def execute(self, game):
-        if self.decision == "y":    # If they chose to place down the card that they picked up
+        if self.decision == "Yes":    # If they chose to place down the card that they picked up
             game.discard_pile.append(game.player_list[game.turn].deck[-1])
             game.player_list[game.turn].deck.pop(-1)
             return Response(game, "Executed")
@@ -25,13 +25,6 @@ class PlaceCard:
         self.colour = kw.get("colour")
 
     def execute(self, game):
-        #if (game.player_list[game.turn].deck[self.choice].colour == game.discard_pile[-1].colour) or \
-        #        (game.player_list[game.turn].deck[self.choice].value == game.discard_pile[-1].value):
-        #
-        #    game.discard_pile.append(game.player_list[game.turn].deck[self.choice])
-        #    game.player_list[game.turn].deck.pop(self.choice)
-        #    return Response(game, "Executed")
-
         if game.player_list[game.turn].deck[self.choice].colour == None:      # If they chose a wild card
             # Replaces the wild card's 'None' colour with the colour chosen and places that card down
             game.player_list[game.turn].deck[self.choice].colour = self.colour
@@ -41,21 +34,20 @@ class PlaceCard:
 
         return Response(game, "Executed")
 
-
 class DrawCard:
     def __init__(self):
-        self.temp = None
+        pass
 
     def execute(self, game):
-        # The draw card method from the Deck object inside the game object is called
+        # The draw card method from the Deck object inside the game object is called. Current P's deck is the parameter
         game.player_list[game.turn].deck = game.dk.draw_card(game.player_list[game.turn].deck)  # Draw 1 card
 
-        # If the card you drew is valid it asks the user if they want to place it down immediately -
+        # If the card you drew is valid the player will be asked if they want to place it down immediately -
         if game.player_list[game.turn].deck[-1].colour == game.discard_pile[-1].colour or \
                 game.player_list[game.turn].deck[-1].value == game.discard_pile[-1].value:
-            return Response(game, "confirm")
+            return Response(game, "Confirm")
 
-        else:   # If they card they drew cannot be placed down, it increments the turn so the game moves on
+        else:   # If the card they drew cannot be placed down, it increments the turn so the game moves on
             game.turn = game.next_turn()
             return Response(game, None)
 
