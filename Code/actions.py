3b717d320634b39fba725b96a0095590bd3c03cc
Fuhou03 <68759675+Fuhou03@users.noreply.github.com>
2022-02-25 22:57:46 +0000

    Added uno button

--- Code/actions.py
+++ Code/actions.py
@@ -4,7 +4,52 @@ class Response:
         self.payload = payload      # payload -  might be None - this is for additional data
 
 
-class Decision:
+class PlaceCard:    # Used to tell the server which card you want to place down
+    def __init__(self, choice, **kw):
+        self.choice = choice    # A number representing the player's choice
+        self.colour = kw.get("colour")
+        self.pressed_uno = None
+
+    def execute(self, game):    # The server calls this method once it receives the object
+        # Checks the colour of the card that the player just picked. A wildcard would have no colour.
+        if game.player_list[game.turn].deck[self.choice].colour is None:      # If they chose a wild card
+            # Replaces the wild card's 'None' colour with the colour chosen and places that card down
+            game.player_list[game.turn].deck[self.choice].colour = self.colour
+
+        # If it's not a wild card then it simply places the card down
+        game.discard_pile.append(game.player_list[game.turn].deck[self.choice])     # Add the card to the discard pile
+        game.player_list[game.turn].deck.pop(self.choice)       # Remove it from your deck
+
+        if len(game.player_list[game.turn].deck) == 1 and self.pressed_uno is True:  # 1 card remaining and they pressed the uno button already
+            game.pressed_uno = [game.turn, True]    # Use a list so we know which player pressed Uno
+        else:
+            game.pressed_uno = None
+
+        return Response(game, "Executed")
+
+
+class DrawCard:     # This is sent to the server to tell them that you want to draw a card
+    def __init__(self):
+        pass
+
+    def execute(self, game):
+        # The draw card method from the Deck object inside the game object is called. Current P's deck is the parameter
+        game.player_list[game.turn].deck = game.dk.draw_card(game.player_list[game.turn].deck)  # Draw 1 card
+
+        if game.pressed_uno is not None:    # If the previous player pressed the Uno button
+            game.pressed_uno = None     # Reset it
+
+        # If the card you drew is valid the player will be asked if they want to place it down immediately -
+        if game.player_list[game.turn].deck[-1].colour == game.discard_pile[-1].colour or \
+                game.player_list[game.turn].deck[-1].value == game.discard_pile[-1].value:
+            return Response(game, "Confirm")
+
+        else:   # If the card they drew cannot be placed down, it increments the turn so the game moves on
+            game.turn = game.next_turn()
+            return Response(game, None)
+
+
+class Decision:     # This is sent to the server to tell them whether to place the picked up card down, or not
     def __init__(self, decision):
         self.decision = decision
 
@@ -15,39 +60,6 @@ class Decision:
             return Response(game, "Executed")
 
         else:   # Moves onto the next player without placing the card down
-            game.turn = game.next_turn()
-            return Response(game, None)
-
-
-class PlaceCard:
-    def __init__(self, choice, **kw):
-        self.choice = choice    # A number representing the player's choice
-        self.colour = kw.get("colour")
-
-    def execute(self, game):
-        if game.player_list[game.turn].deck[self.choice].colour == None:      # If they chose a wild card
-            # Replaces the wild card's 'None' colour with the colour chosen and places that card down
-            game.player_list[game.turn].deck[self.choice].colour = self.colour
-
-        game.discard_pile.append(game.player_list[game.turn].deck[self.choice])     # Add the card to the discard pile
-        game.player_list[game.turn].deck.pop(self.choice)       # Remove it from your deck
-
-        return Response(game, "Executed")
-
-class DrawCard:
-    def __init__(self):
-        pass
-
-    def execute(self, game):
-        # The draw card method from the Deck object inside the game object is called. Current P's deck is the parameter
-        game.player_list[game.turn].deck = game.dk.draw_card(game.player_list[game.turn].deck)  # Draw 1 card
-
-        # If the card you drew is valid the player will be asked if they want to place it down immediately -
-        if game.player_list[game.turn].deck[-1].colour == game.discard_pile[-1].colour or \
-                game.player_list[game.turn].deck[-1].value == game.discard_pile[-1].value:
-            return Response(game, "Confirm")
-
-        else:   # If the card they drew cannot be placed down, it increments the turn so the game moves on
             game.turn = game.next_turn()
             return Response(game, None)
 
