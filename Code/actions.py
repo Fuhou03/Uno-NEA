ca9991febf41e8ae29c650916efe9d501ad6925c
Fuhou03 <68759675+Fuhou03@users.noreply.github.com>
2022-02-26 16:58:10 +0000

    Finished Uno function

--- Code/actions.py
+++ Code/actions.py
@@ -8,22 +8,32 @@ class PlaceCard:    # Used to tell the server which card you want to place down
     def __init__(self, choice, **kw):
         self.choice = choice    # A number representing the player's choice
         self.colour = kw.get("colour")
-        self.pressed_uno = None
+        self.pressed_uno = False
 
     def execute(self, game):    # The server calls this method once it receives the object
-        # Checks the colour of the card that the player just picked. A wildcard would have no colour.
-        if game.player_list[game.turn].deck[self.choice].colour is None:      # If they chose a wild card
-            # Replaces the wild card's 'None' colour with the colour chosen and places that card down
+        # Checks if the player picked a wild card (they have no colour)
+        if game.player_list[game.turn].deck[self.choice].colour is None:
+            # Replaces the wild card's 'None' colour with the colour chosen
             game.player_list[game.turn].deck[self.choice].colour = self.colour
 
-        # If it's not a wild card then it simply places the card down
+        # Places the card down
         game.discard_pile.append(game.player_list[game.turn].deck[self.choice])     # Add the card to the discard pile
         game.player_list[game.turn].deck.pop(self.choice)       # Remove it from your deck
 
-        if len(game.player_list[game.turn].deck) == 1 and self.pressed_uno is True:  # 1 card remaining and they pressed the uno button already
-            game.pressed_uno = [game.turn, True]    # Use a list so we know which player pressed Uno
-        else:
-            game.pressed_uno = None
+        if len(game.player_list[game.turn].deck) == 1 and self.pressed_uno is True:
+            # 1 card remaining and they pressed the uno button already
+            game.pressed_uno = game.turn    # So we know which player pressed uno
+
+        elif len(game.player_list[game.turn].deck) == 1 and self.pressed_uno is False:  # They forgot to say uno
+            game.forgot_player = game.turn
+
+        elif len(game.player_list[game.turn].deck) > 1 and self.pressed_uno is True:
+            game.pressed_uno = False    # If the previous player pressed the Uno button
+            # Reset this variable so that it doesn't display to users that a player said 'Uno', in the next turn
+
+        elif game.forgot_player is not False:     # If the previous player forgot to press the Uno button
+            game.forgot_player = False
+            # Reset this variable so that it doesn't display to users that a player has forgotten, in the next turn
 
         return Response(game, "Executed")
 
@@ -33,11 +43,12 @@ class DrawCard:     # This is sent to the server to tell them that you want to d
         pass
 
     def execute(self, game):
-        # The draw card method from the Deck object inside the game object is called. Current P's deck is the parameter
-        game.player_list[game.turn].deck = game.dk.draw_card(game.player_list[game.turn].deck)  # Draw 1 card
+        game.draw_card(game.player_list[game.turn].deck)    # Draw 1 card
 
-        if game.pressed_uno is not None:    # If the previous player pressed the Uno button
-            game.pressed_uno = None     # Reset it
+        if game.pressed_uno is not False:    # If the previous player pressed the Uno button
+            game.pressed_uno = False     # Reset
+        elif game.forgot_player is not False:
+            game.forgot_player = False  # Reset
 
         # If the card you drew is valid the player will be asked if they want to place it down immediately -
         if game.player_list[game.turn].deck[-1].colour == game.discard_pile[-1].colour or \
