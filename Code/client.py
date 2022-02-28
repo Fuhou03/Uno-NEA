3c86e91cbb58952b96fb59529c6894f748b711db
Fuhou03 <68759675+Fuhou03@users.noreply.github.com>
2022-02-28 10:14:17 +0000

    Update client.py

--- Code/client.py
+++ Code/client.py
@@ -7,8 +7,7 @@ class Network:
     def __init__(self):
         self.client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         self.ip = socket.gethostbyname(socket.gethostname())
-        #self.ip = "178.79.144.126"
-
+       
         self.port = 5555
 
         self.addr = (self.ip, self.port)
